1819d2c727403278a1143f3722ad3ecda5cd91aa
Ricardo Valério <jricvalerio@gmail.com>
2015-04-18 00:28:50 +0100

    :rocket:

--- meteor-tutorial/meteor-tutorial.js
+++ meteor-tutorial/meteor-tutorial.js
@@ -22,20 +22,45 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   // This code only runs on the client
 
-  Template.body.helpers({
-    tasks: function () {
-      /**
-      * STEP 1
-      */
-      // return Tasks.find({});
+/************************** TEMPLATE BODY *********************************/
 
-      /**
-      * STEP 2
-      * Show newest tasks first
-      */
-      return Tasks.find({}, {sort: {createdAt: -1}});
-    }
-  });
+  // Template.body.helpers({
+  //   tasks: function () {
+  //     /**
+  //     * STEP 1
+  //     */
+  //     // return Tasks.find({});
+
+  //     *
+  //     * STEP 2
+  //     * Show newest tasks first
+
+  //     return Tasks.find({}, {sort: {createdAt: -1}});
+  //   }
+  // });
+
+
+    // Replace the existing Template.body.helpers
+    Template.body.helpers({
+      tasks: function () {
+        if (Session.get("hideCompleted")) {
+          // If hide completed is checked, filter tasks
+          return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+        } else {
+          // Otherwise, return all of the tasks
+          return Tasks.find({}, {sort: {createdAt: -1}});
+        }
+      },
+
+      hideCompleted: function () {
+        return Session.get("hideCompleted");
+      },
+
+      incompleteCount: function () {
+        return Tasks.find({checked: {$ne: true}}).count();
+      }
+
+    });
 
   Template.body.events({
     "submit .new-task": function (event) {
@@ -51,10 +76,21 @@ if (Meteor.isClient) {
       */
       var text = event.target.text.value;
 
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
+      // Tasks.insert({
+      //   text: text,
+      //   createdAt: new Date(),            // current time
+      //   owner: Meteor.userId(),           // _id of logged in user
+      //   username: Meteor.user().username  // username of logged in user
+      // });
+      Meteor.call("addTask", text);
+
+      /**
+          Getting information about the logged-in user:
+
+          In your HTML, you can use the built-in {{currentUser}} helper to check if a user is logged in and get information about them. For example, {{currentUser.username}} will display the logged in user's username.
+
+          In your JavaScript code, you can use Meteor.userId() to get the current user's _id, or Meteor.user() to get the whole user document.
+      */
 
       // Clear form
       event.target.text.value = "";
@@ -65,8 +101,17 @@ if (Meteor.isClient) {
        * form submit action since we have already handled it.
        */
        return false;
-     }
+     },
+
+     "change .hide-completed input": function (event) {
+        Session.set("hideCompleted", event.target.checked);
+      }
+
+
    });
+
+
+/************************** TEMPLATE TASK *********************************/
 
 
   /*
@@ -93,7 +138,8 @@ if (Meteor.isClient) {
     */
     "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+      // Tasks.update(this._id, {$set: {checked: ! this.checked}});
+      Meteor.call("setChecked", this._id, ! this.checked);
     },
 
     /*
@@ -103,8 +149,15 @@ if (Meteor.isClient) {
       which item to remove from the collection.
     */
     "click .delete": function () {
-      Tasks.remove(this._id);
+      // Tasks.remove(this._id);
+      Meteor.call("deleteTask", this._id);
     }
+  });
+
+
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 
 }
@@ -117,3 +170,27 @@ if (Meteor.isClient) {
 // db.tasks.insert({ text: "Clean the house!", createdAt: new Date() });
 // db.tasks.insert({ text: "Kiss my nice!", createdAt: new Date() });
 // db.tasks.insert({ text: "Be happy!", createdAt: new Date() });
+
+
+// At the bottom of simple-todos.js, outside of the client-only block
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});
