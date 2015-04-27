a6d636005c78f7991375cbbd50be595872e88dff
Ricardo Valério <jricvalerio@gmail>
2015-04-27 19:44:47 +0100

    add methods to have latency compensation and other sweet stuff

--- 0!UNDERSTOOD/try-meteor-tutorial-todosapp-official-site/try-meteor-tutorial-todosapp-official-site.js
+++ 0!UNDERSTOOD/try-meteor-tutorial-todosapp-official-site/try-meteor-tutorial-todosapp-official-site.js
@@ -36,12 +36,13 @@ if (Meteor.isClient) {
       // The event handler gets an argument called event that has some information about the event that was triggered. In this case event.target is our form element, and we can get the value of our input with event.target.text.value. You can see all of the other properties of the event object by adding a console.log(event) and inspecting the object in your browser console.
       console.log(event);
 
-      Tasks.insert({
-        text: text,
-        createdAt: new Date(),
-        owner: Meteor.userId(),           // _id of logged in user
-        username: Meteor.user().username  // username of logged in user
-      });
+      Meteor.call("addTask", text);
+      // Tasks.insert({
+      //   text: text,
+      //   createdAt: new Date(),
+      //   owner: Meteor.userId(),           // _id of logged in user
+      //   username: Meteor.user().username  // username of logged in user
+      // });
 
       //clean form input
       event.target.text.value = "";
@@ -70,11 +71,13 @@ if (Meteor.isClient) {
     "click .toggle-checked": function ()
     {
       // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+      Meteor.call("setChecked", this._id, ! this.checked);
+      // Tasks.update(this._id, {$set: {checked: ! this.checked}});
     },
     "click .delete": function ()
     {
-      Tasks.remove(this._id);
+      Meteor.call("deleteTask", this._id);
+      // Tasks.remove(this._id);
     }
   });
 
@@ -82,8 +85,60 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 
-
 }
+
+
+
+
+/**
+    When we insert tasks into the database, we can now securely verify that the user is logged in, that the createdAt field is correct, and that the owner and username fields are correct and the user isn't impersonating anyone.
+
+    We can add extra validation logic to setChecked and deleteTask in later steps when users can make tasks private.
+
+    Our client code is now more separated from our database logic. Instead of a lot of stuff happening inside our event handlers, we now have methods that can be called from anywhere.
+*/
+
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});
+
+/**
+So why do we want to define our methods on the client and on the server? We do this to enable a feature called latency compensation.
+
+When you call a method on the client using Meteor.call, two things happen in parallel:
+
+    The client sends a request to the server to run the method in a secure environment, just like an AJAX request would work
+    A simulation of the method runs directly on the client to attempt to predict the outcome of the server call using the available information
+
+What this means is that a newly created task actually appears on the screen before the result comes back from the server.
+
+If the result from the server comes back and is consistent with the simulation on the client, everything remains as is. If the result on the server is different from the result of the simulation on the client, the UI is patched to reflect the actual state of the server.
+
+With Meteor methods and latency compensation, you get the best of both worlds — the security of server code and no round-trip delay.
+
+*/
+
+
+
+
 
 if (Meteor.isServer) {
 
