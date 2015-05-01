c97dd5d002e3f670e2f302fc55d8ceb212c29000
Ricardo Valério <jricvalerio@gmail.com>
2015-05-01 21:21:36 +0100

    :rokect:

---
+++ 0!UNDERSTOOD/meteor-chat-tutorial/README.md
@@ -0,0 +1,3 @@
+#Meteor chat application
+
+Video tutorial vailable on [Youtube](https://www.youtube.com/playlist?list=PLoy-bs-bDKTiQq4EUMXwndSYvhD9Ov638).
---
+++ 0!UNDERSTOOD/meteor-chat-tutorial/chat.js
@@ -0,0 +1,62 @@
+Messages = new Mongo.Collection('messages');
+if (Meteor.isClient) {
+  Meteor.subscribe('messages', 5);
+
+  Template.hello.helpers({
+    messages: function () {
+      return Messages.find({}, {
+        sort: { timestamp: -1 }
+      });
+    }
+  });
+
+  Template.hello.events({
+    'submit .chat-form': function (evt) {
+      evt.preventDefault();
+      var text = evt.target.message.value;
+      console.log(text);
+      Meteor.call('insertMessage', text, function(err, result) {
+        if (err) {
+          console.log(err);
+          alert(err.reason);
+        } else {
+          console.log('Message inserted with ID: ', result);
+          evt.target.message.value = '';
+        }
+      });
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.methods({
+    insertMessage: function(text) {
+      if (!this.userId) {
+        throw new Meteor.Error("logged-out",
+          "The user must be logged in to post a message.");
+      }
+      var user = Meteor.users.findOne(this.userId);
+      return Messages.insert({
+        userId: this.userId,
+        username: user.username,
+        text: text,
+        timestamp: Date.now()
+      });
+    }
+  });
+
+  Meteor.publish('messages', function(limit) {
+    //publish messages just for logged in user
+    if (this.userId) {
+      return Messages.find({}, {
+        limit: limit || 5,
+        sort: { timestamp: -1 }
+      });
+    }
+    this.ready();
+  });
+}
