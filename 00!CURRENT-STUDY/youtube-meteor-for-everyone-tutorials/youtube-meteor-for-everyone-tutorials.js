78039d0886175f2f1e1684d17e6236c49384ae2e
Ricardo Valério <jricvalerio@gmail.com>
2015-05-02 01:47:36 +0100

    :rocket:

--- 00!CURRENT-STUDY/youtube-meteor-for-everyone-tutorials/youtube-meteor-for-everyone-tutorials.js
+++ 00!CURRENT-STUDY/youtube-meteor-for-everyone-tutorials/youtube-meteor-for-everyone-tutorials.js
@@ -2,12 +2,46 @@ Resolutions = new Mongo.Collection("resolutions");
 
 if (Meteor.isClient) {
 
+	Template.body.events({
+		"submit .new-resolution": function(event) {
+			event.preventDefault();
 
-Template.resolutions.helpers({
-  resolutions : function() {
-    return Resolutions.find();
-  }
-});
+			// get the value of input named "title"
+			var title = event.target.title.value;
+
+			// return if the input is void
+			if (!title.length) return false;
+
+			// otherwise insert into the mongodb
+			Resolutions.insert({
+				title     : title,
+				createdAt : new Date()
+			});
+
+			// clean the input field
+			title = "";
+		}
+	});
+
+	Template.resolutions.helpers({
+	  resolutions : function() {
+	    return Resolutions.find();
+	  }
+	});
+
+	Template.resolutions.events({
+		"click button.delete": function() {
+			// "this" referes the resolution that was clicked
+			Resolutions.remove(this._id);
+		},
+		"click .toggle-checked": function() {
+			Resolutions.update(this._id, {
+					$set: {
+						checked: !this.checked
+					}
+			});
+		}
+	});
 
 
 }
